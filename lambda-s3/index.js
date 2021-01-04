29f5c30d1d90c7514b293529433f2dc3044b0bd2
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2021-01-03 18:39:14 -0800

    image resize issue fixed

--- lambda-s3/index.js
+++ lambda-s3/index.js
@@ -44,7 +44,7 @@ exports.handler = async (event, context, callback) => {
   }
 
   // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
-  const width = 200;
+  const width = 50;
 
   // Use the Sharp module to resize the image and save in a buffer.
   try {
